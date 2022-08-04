b2e2bd1d11c73bca12c16454c307094aca0e54ab
romanovskivadim <67336157+romanovskivadim@users.noreply.github.com>
2022-08-04 18:33:06 +0300

    add build

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,10 @@
 import { iframe } from "./Iframe/iframe";
 import { sliderComponent } from "./Slider/slider";
 import { refreshComponent } from "./refreshComponent/refreshComponent";
+import { pdfViewer } from "./PDFViewer/PDFViewerComponent";
 export declare const customComponents: {
     iframe: typeof iframe;
     sliderComponent: typeof sliderComponent;
     refreshComponent: typeof refreshComponent;
+    pdfViewer: typeof pdfViewer;
 };
