273fb0322c11f35b21c08a8baa001d50ef981c88
romanovskivadim <67336157+romanovskivadim@users.noreply.github.com>
2022-08-04 11:36:48 +0300

    add File component

---
+++ types/FileComponent/FileComponent.edit.display.d.ts
@@ -0,0 +1,35 @@
+export declare const FileComponentEditDisplay: ({
+    key: string;
+    ignore: boolean;
+    type?: undefined;
+    input?: undefined;
+    label?: undefined;
+    weight?: undefined;
+    validate?: undefined;
+} | {
+    key: string;
+    ignore?: undefined;
+    type?: undefined;
+    input?: undefined;
+    label?: undefined;
+    weight?: undefined;
+    validate?: undefined;
+} | {
+    type: string;
+    input: boolean;
+    label: string;
+    key: string;
+    weight: number;
+    validate: {
+        required: boolean;
+    };
+    ignore?: undefined;
+} | {
+    type: string;
+    label: string;
+    weight: number;
+    key: string;
+    ignore?: undefined;
+    input?: undefined;
+    validate?: undefined;
+})[];
