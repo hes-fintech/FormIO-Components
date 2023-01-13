9fe272e786e320b248da1d7ce561fe670a65f83b
s.tkachuk <sergej.tkachuk@hiendsys.com>
2023-01-13 15:28:25 +0300

    added components to FormioBuilderComponent  -select  -datagrid  -panel  -checkbox

--- src/FormioBuilderComponent/ComponentsSettings.ts
+++ src/FormioBuilderComponent/ComponentsSettings.ts
@@ -890,4 +890,254 @@ export const componentsSettings = {
         ]
       },
   ],
+    datagrid: [
+    {
+      key: 'api',
+      ignore: true,
+    },
+    {
+      key: 'layout',
+      ignore: true,
+    },
+    {
+      key: 'logic',
+      ignore: true,
+    },
+    {
+      key: 'data',
+      ignore: true,
+    },
+    {
+      key: 'display',
+      ignore: true,
+    },
+    {
+      weight: 0,
+      key: 'customDisplay',
+      type: 'textfield',
+      label: 'Display',
+      components: [
+        {
+          weight: 0,
+          type: 'textfield',
+          input: true,
+          key: 'key',
+          label: 'Component key in form',
+          validate: {
+            required: true,
+          },
+        },
+        {
+          weight: 1,
+          type: 'textfield',
+          input: true,
+          key: 'label',
+          label: 'Label',
+          validate: {
+            required: true,
+          },
+        },
+      ],
+    },
+  ],
+    checkbox: [
+    {
+      key: 'display',
+      ignore: true,
+    },
+    {
+      key: 'data',
+      ignore: true,
+    },
+    {
+      key: 'api',
+      ignore: true,
+    },
+    {
+      key: 'conditional',
+      ignore: true,
+    },
+    {
+      key: 'logic',
+      ignore: true,
+    },
+    {
+      key: 'attributes',
+      ignore: true,
+    },
+    {
+      key: 'layout',
+      ignore: true,
+    },
+    {
+      key: 'validation',
+      ignore: true,
+    },
+    {
+      weight: 0,
+      key: 'customDisplay',
+      type: 'textfield',
+      label: 'Display',
+      components: [
+        {
+          weight: 0,
+          type: 'textfield',
+          input: true,
+          key: 'key',
+          label: 'Component key in form',
+          placeholder: 'Field Label',
+          validate: {
+            required: true,
+          },
+        },
+        {
+          weight: 1,
+          type: 'textfield',
+          input: true,
+          key: 'label',
+          label: 'Label',
+          tooltip:
+            'The label for this field that will appear next to it.',
+          placeholder: 'Field Label',
+          validate: {
+            required: true,
+          },
+        },
+      ],
+    },
+    {
+      label: 'Validation',
+      key: 'customValidation',
+      weight: 2,
+      components: [
+        {
+          weight: 10,
+          type: 'checkbox',
+          label: 'Required',
+          tooltip:
+            'A required field must be filled in before the form can be submitted.',
+          key: 'validate.required',
+          input: true,
+        },
+        {
+          weight: 190,
+          type: 'textfield',
+          input: true,
+          key: 'errorLabel',
+          label: 'Error Label',
+          placeholder: 'Error Label',
+          tooltip: 'The label for this field when an error occurs.',
+        },
+        {
+          weight: 200,
+          key: 'validate.customMessage',
+          label: 'Custom Error Message',
+          placeholder: 'Custom Error Message',
+          type: 'textfield',
+          tooltip: 'Error message displayed if any error occurred.',
+          input: true,
+        },
+      ],
+    },
+  ],
+    select: [
+    {
+      key: 'api',
+      ignore: true,
+    },
+    {
+      key: 'logic',
+      ignore: true,
+    },
+    {
+      key: 'layout',
+      ignore: true,
+    },
+    {
+      key: 'data',
+      ignore: true,
+    },
+    {
+      key: 'display',
+      ignore: true,
+    },
+    {
+      weight: 0,
+      key: 'customDisplay',
+      type: 'textfield',
+      label: 'Display',
+      components: [
+        {
+          weight: 0,
+          type: 'textfield',
+          input: true,
+          key: 'key',
+          label: 'Component key in form',
+          placeholder: 'Field Label',
+          validate: {
+            required: true,
+          },
+        },
+        {
+          weight: 1,
+          type: 'textfield',
+          input: true,
+          key: 'label',
+          label: 'Label',
+          placeholder: 'Field Label',
+          validate: {
+            required: true,
+          },
+        },
+      ],
+    },
+    {
+      weight: 1,
+      key: 'customData',
+      type: 'textfield',
+      label: 'Data',
+      components: [
+        {
+          type: 'datagrid',
+          input: true,
+          label: 'Data Source Values',
+          key: 'data.values',
+          tooltip:
+            'Values to use as the data source. Labels are shown in the select field. Values are the corresponding values saved with the submission.',
+          weight: 10,
+          reorder: true,
+          defaultValue: [{ label: '', value: '' }],
+          components: [
+            {
+              label: 'Label',
+              key: 'label',
+              input: true,
+              type: 'textfield',
+            },
+            {
+              label: 'Value',
+              key: 'value',
+              input: true,
+              type: 'textfield',
+              allowCalculateOverride: true,
+              calculateValue: 'value = _.camelCase(row.label);',
+            },
+          ],
+          conditional: {
+            json: { '===': [{ var: 'data.dataSrc' }, 'values'] },
+          },
+        },
+        {
+          type: 'checkbox',
+          input: true,
+          weight: 21,
+          key: 'searchEnabled',
+          label: 'Enable Static Search',
+          defaultValue: true,
+          tooltip:
+            'When checked, the select dropdown will allow for searching within the static list of items provided.',
+        },
+      ],
+    },
+  ],
 }
--- src/FormioBuilderComponent/FormioBuilderComponent.edit.display.ts
+++ src/FormioBuilderComponent/FormioBuilderComponent.edit.display.ts
@@ -1,130 +1,150 @@
 export const formioBuilderComponentEditDisplay = [
-  {
-      type: 'textfield',
-      input: false,
-      label: 'Submission key',
-      key: 'key',
-      defaultValue: 'container',
-      weight: 0,
-  },
-  {
-    weight: 1,
-    type: 'checkbox',
-    label: 'Text field component',
-    key: 'textFieldComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 2,
-    type: 'checkbox',
-    label: 'Text area component',
-    key: 'textAreaComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 3,
-    type: 'checkbox',
-    label: 'Email component',
-    key: 'emailComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 4,
-    type: 'checkbox',
-    label: 'Number component',
-    key: 'numberComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 5,
-    type: 'checkbox',
-    label: 'File component',
-    key: 'fileComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 6,
-    type: 'checkbox',
-    label: 'Date/Time',
-    key: 'dateTimeComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 7,
-    type: 'checkbox',
-    label: 'Panel',
-    key: 'panelComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 8,
-    type: 'checkbox',
-    label: 'Columns',
-    key: 'columnsComponent',
-    defaultValue: true,
-    input: true
-  },
-  {
-    weight: 9,
-    type: 'checkbox',
-    label: 'Disabled',
-    tooltip: 'Disable the component in form.',
-    key: 'disabled',
-    input: true
-  },
-  {
-      key: 'placeholder',
-      ignore: true,
-  },
-  {
-      key: 'labelPosition',
-      ignore: true,
-  },
-  {
-      key: 'tooltip',
-      ignore: true,
-  },
-  {
-      key: 'tabindex',
-      ignore: true,
-  },
-  {
-      key: 'autofocus',
-      ignore: true,
-  },
-  {
-      key: 'hideLabel',
-      ignore: true,
-  },
-  {
-      key: 'tableView',
-      ignore: true,
-  },
-  {
-      key: 'autofocus',
-      ignore: true,
-  },
-  {
-      key: 'label',
-      ignore: true,
-  },
-  {
-      key: 'customClass',
-      ignore: true,
-  },
-  {
-      key: 'hidden',
-      ignore: true,
-  },
-  {
-      key: 'modalEdit',
-      ignore: true,
-  },
+    {
+        type: 'textfield',
+        input: false,
+        label: 'Submission key',
+        key: 'key',
+        defaultValue: 'container',
+        weight: 0,
+    },
+    {
+        weight: 1,
+        type: 'checkbox',
+        label: 'Text field component',
+        key: 'textFieldComponent',
+        defaultValue: true,
+        input: true,
+    },
+    {
+        weight: 2,
+        type: 'checkbox',
+        label: 'Text area component',
+        key: 'textAreaComponent',
+        defaultValue: true,
+        input: true,
+    },
+    {
+        weight: 3,
+        type: 'checkbox',
+        label: 'Email component',
+        key: 'emailComponent',
+        defaultValue: true,
+        input: true,
+    },
+    {
+        weight: 4,
+        type: 'checkbox',
+        label: 'Number component',
+        key: 'numberComponent',
+        defaultValue: true,
+        input: true,
+    },
+    {
+        weight: 5,
+        type: 'checkbox',
+        label: 'File component',
+        key: 'fileComponent',
+        input: true,
+    },
+    {
+        weight: 6,
+        type: 'checkbox',
+        label: 'Date/Time',
+        key: 'dateTimeComponent',
+        input: true,
+    },
+    {
+        weight: 7,
+        type: 'checkbox',
+        label: 'Panel',
+        key: 'panelComponent',
+        input: true,
+    },
+    {
+        weight: 8,
+        type: 'checkbox',
+        label: 'Columns',
+        key: 'columnsComponent',
+        input: true,
+    },
+    {
+        weight: 9,
+        type: 'checkbox',
+        label: 'Disabled',
+        tooltip: 'Disable the component in form.',
+        key: 'disabled',
+        input: true,
+    },
+    {
+        weight: 10,
+        type: 'checkbox',
+        label: 'Select',
+        tooltip: 'Select field component',
+        key: 'selectComponent',
+        input: true,
+    },
+    {
+        weight: 11,
+        type: 'checkbox',
+        label: 'Checkbox',
+        tooltip: 'Checkbox field component',
+        key: 'checkboxComponent',
+        input: true,
+    },
+    {
+        weight: 12,
+        type: 'checkbox',
+        label: 'Data Grid',
+        tooltip: 'Data Grid component',
+        key: 'datagridComponent',
+        input: true,
+    },
+    {
+        key: 'placeholder',
+        ignore: true,
+    },
+    {
+        key: 'labelPosition',
+        ignore: true,
+    },
+    {
+        key: 'tooltip',
+        ignore: true,
+    },
+    {
+        key: 'tabindex',
+        ignore: true,
+    },
+    {
+        key: 'autofocus',
+        ignore: true,
+    },
+    {
+        key: 'hideLabel',
+        ignore: true,
+    },
+    {
+        key: 'tableView',
+        ignore: true,
+    },
+    {
+        key: 'autofocus',
+        ignore: true,
+    },
+    {
+        key: 'label',
+        ignore: true,
+    },
+    {
+        key: 'customClass',
+        ignore: true,
+    },
+    {
+        key: 'hidden',
+        ignore: true,
+    },
+    {
+        key: 'modalEdit',
+        ignore: true,
+    },
 ];
