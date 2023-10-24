ca99a214b38b216615ce98a2002d2efd280de600
Vadim Romanovskij <67336157+romanovskivadim@users.noreply.github.com>
2023-10-24 15:32:08 +0200

    Fix validation for slider

---
+++ types/Slider/sliderComponent.edit.validation.d.ts
@@ -0,0 +1,19 @@
+export declare const sliderComponentValidation: ({
+    key: string;
+    ignore: boolean;
+    type?: undefined;
+    label?: undefined;
+    input?: undefined;
+    placeholder?: undefined;
+    tooltip?: undefined;
+    weight?: undefined;
+} | {
+    type: string;
+    label: string;
+    key: string;
+    input: boolean;
+    placeholder: string;
+    tooltip: string;
+    weight: number;
+    ignore?: undefined;
+})[];
